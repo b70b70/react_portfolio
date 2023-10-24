7d95d3808d980ea95c2ce001b8edc7e9ac150a44
Ali <134099889+b70b70@users.noreply.github.com>
2023-10-24 21:29:17 +0100

    added link tag for nav bar in the header directs correctly

--- react_portfolio/src/components/Header.js
+++ react_portfolio/src/components/Header.js
@@ -1,24 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
-  const handleCustomNavigation = (destination) => {
-    // Your custom navigation logic here
-    console.log(`Navigating to: ${destination}`);
-  };
-
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link" onClick={() => handleCustomNavigation('home')}>Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={() => handleCustomNavigation('projects')}>Projects</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" onClick={() => handleCustomNavigation('contact')}>Contact</a>
-        </li>
-      </ul>
+      <Link to="/" className="navbar-brand">My Portfolio</Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/projects" className="nav-link">Projects</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-link">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
