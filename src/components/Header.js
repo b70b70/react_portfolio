9149d38f1e79c11fb468a12ddaa056f549a82960
Ali <134099889+b70b70@users.noreply.github.com>
2023-10-26 01:25:51 +0100

    link in nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link to="/" className="navbar-brand">My Portfolio</Link>
+      <Link to="/react_portfolio" className="navbar-brand">My Portfolio</Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
